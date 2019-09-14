e82bd68b6df70ad1c050c3282f7aacde14587471
konradek281997 <konradek281997@users.noreply.github.com>
2019-09-15 00:28:27 +0200

    Update MainWindow.xaml.cs

--- Planowanie_zamowienia_w_PC/Planowanie_zamowienia_w_PC/MainWindow.xaml.cs
+++ Planowanie_zamowienia_w_PC/Planowanie_zamowienia_w_PC/MainWindow.xaml.cs
@@ -20,17 +20,47 @@ namespace Planowanie_zamowienia_w_PC
     /// </summary>
     public partial class MainWindow : Window
     {
-        
+
         public MainWindow()
         {
             InitializeComponent();
+            Lista_Produktow.ItemsSource = Produkt;
+            Resources["ListaProduktow"] = Produkt;
+            Dodawanie();
         }
 
+        public Dictionary<A, B> Produkt = new Dictionary<A, B>();
+
+        public void Dodawanie()
+        {
+            Produkt.Add(new A("witaminy"), new B(57.60));
+            Produkt.Add(new A("Kolagen"), new B(112));
+            
+            Lista_Produktow.Items.Refresh();
+        }
+
+        public class A
+        {
+            public string Nazwa { get; set; }
+            public A(string okNazwa)
+            {
+                Nazwa = okNazwa;
+            }
+        }
+        public class B
+        {
+            public double Cena { get; set; }
+            public B(double okCena)
+            {
+                Cena = okCena;
+            }
+        }
         private void Przycisk_Rozpocznij_Click(object sender, RoutedEventArgs e)
         {
             Wczytaj();
             Znikanie_ekranow();
         }
+
         private void Wczytaj()
         {
             Wczytanie_produktow.Wybor_wczytywanego_produktu();
@@ -41,5 +71,11 @@ namespace Planowanie_zamowienia_w_PC
             Ekran_Powitalny.Visibility = Visibility.Collapsed;
         }
 
+        private void Przycisk_Dodaj_Do_Koszyka_Click(object sender, RoutedEventArgs e)
+        {
+            Dodawanie();
+            Lista_Produktow.Items.Refresh();
+        }
     }
 }
+
