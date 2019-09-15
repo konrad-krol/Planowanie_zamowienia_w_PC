e6394766ae0129c0f484a9a87c4e096ed92b9df9
konradek281997 <konradek281997@users.noreply.github.com>
2019-09-15 23:28:12 +0200

    Update Bindowanie.cs

--- Planowanie_zamowienia_w_PC/Planowanie_zamowienia_w_PC/Bindowanie.cs
+++ Planowanie_zamowienia_w_PC/Planowanie_zamowienia_w_PC/Bindowanie.cs
@@ -8,62 +8,38 @@ namespace Planowanie_zamowienia_w_PC
 {
     class Bindowanie
     {
-        public static Dictionary<string, string> Suplementy = new Dictionary<string, string>();
-        public static Dictionary<string, string> Kosmetyki = new Dictionary<string, string>();
-        public static Dictionary<string, string> Urzadzenia = new Dictionary<string, string>();
-        public static Dictionary<string, string> Aloes = new Dictionary<string, string>();
-        public static Dictionary<string, string> Kolagen = new Dictionary<string, string>();
-
-        public string Suplement_Nazwa { get; set; }
-        public string Suplement_Cena { get; set; }
-        public string Kosmetyki_Nazwa { get; set; }
-        public string Kosmetyki_Cena { get; set; }
-        public string Urzadzenia_Nazwa { get; set; }
-        public string Urzadzenia_Cena { get; set; }
-        public string Aloes_Nazwa { get; set; }
-        public string Aloes_Cena { get; set; }
-        public string Kolagen_Nazwa { get; set; }
-        public string Kolagen_Cena { get; set; }
-
-        public Bindowanie(string Suplement_Nazwa_1, string Suplement_Cena_1, string Kosmetyki_Nazwa_1, string Kosmetyki_Cena_1,
-            string Urzadzenia_Nazwa_1, string Urzadzenia_Cena_1, string Aloes_Nazwa_1, string Aloes_Cena_1,
-            string Kolagen_Nazwa_1, string Kolagen_Cena_1)
+        public static Dictionary<Klucz, Zawartosc_1> Suplementy = new Dictionary<Klucz, Zawartosc_1>();
+        public static Dictionary<Klucz, Zawartosc_2> Kosmetyki = new Dictionary<Klucz, Zawartosc_2>();
+        public static Dictionary<Klucz, Zawartosc_2> Urzadzenia = new Dictionary<Klucz, Zawartosc_2>();
+        public static Dictionary<Klucz, Zawartosc_2> Aloes = new Dictionary<Klucz, Zawartosc_2>();
+        public static Dictionary<Klucz, Zawartosc_2> Kolagen = new Dictionary<Klucz, Zawartosc_2>();
+        public class Klucz
         {
-            Suplement_Nazwa = Suplement_Nazwa_1;
-            Suplement_Cena = Suplement_Cena_1;
-            Kosmetyki_Nazwa = Kosmetyki_Nazwa_1;
-            Kosmetyki_Cena = Kosmetyki_Cena_1;
-            Urzadzenia_Nazwa = Urzadzenia_Nazwa_1;
-            Urzadzenia_Cena = Urzadzenia_Cena_1;
-            Aloes_Nazwa = Aloes_Nazwa_1;
-            Aloes_Cena = Aloes_Cena_1;
-            Kolagen_Nazwa = Kolagen_Nazwa_1;
-            Kolagen_Cena = Kolagen_Cena_1;
-        }
-        
-        public static Dictionary<Nazwa,Cena> Produkt = new Dictionary<Nazwa, Cena>();
-
-        public static void Dodawanie()
-        {
-            Produkt.Add(new Nazwa("abc"), new Cena(123));
-            Produkt.Add(new Nazwa("def"), new Cena(456));
-        }
-
-        public class Nazwa
-        {
-            public Nazwa(string name)
+            public string Nazwa { get; set; }
+            public Klucz(string okNazwa)
             {
-                Nazwa1 = name;
+                Nazwa = okNazwa;
             }
-            public string Nazwa1 { get; set; }
         }
-        public class Cena
+        public class Zawartosc_1
         {
-            public Cena(int price)
+            public double? Cena { get; set; }
+            public double? Ilosc { get; set; }
+            public Zawartosc_1(double? okCena, double? okIlosc)
             {
-                Cena1 = price;
+                Cena = okCena;
+                Ilosc = okIlosc;
             }
-            public int Cena1 { get; set; }
         }
+        public class Zawartosc_2
+        {
+            public double? Cena { get; set; }
+            public Zawartosc_2(double? okCena)
+            {
+                Cena = okCena;
+            }
+        }
+
+
     }
 }
