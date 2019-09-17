66bd8d129c2abcc26e7620a7fe469184649f51f2
konradek281997 <konradek281997@users.noreply.github.com>
2019-09-17 12:06:39 +0200

    Update Wczytanie_produktow.cs

--- Planowanie_zamowienia_w_PC/Planowanie_zamowienia_w_PC/Wczytanie_produktow.cs
+++ Planowanie_zamowienia_w_PC/Planowanie_zamowienia_w_PC/Wczytanie_produktow.cs
@@ -20,42 +20,38 @@ namespace Planowanie_zamowienia_w_PC
         private static string sciezka_kolagen_grafit_srebro_platyna = @"C:\Projekty\Planowanie_zamowienia_w_PC\Planowanie_zamowienia_w_PC\Spis_produktow\KOLAGEN_GRAFIT_SREBRO_PLATYNA.txt";
         private static string sciezka_aloe_vera_line = @"C:\Projekty\Planowanie_zamowienia_w_PC\Planowanie_zamowienia_w_PC\Spis_produktow\ALOE_VERA_LINE.txt";
 
-
         public static bool Wybor_wczytywanego_produktu(bool potwierdzenie)
         {
-            Wczytaj_aloes();
-            Wczytaj_kolagen();
-            Wczytaj_kosmetyki();
-            Wczytaj_suplementy();
-            Wczytaj_urzadzenia();
-            zaladuj();
-
             string[] spis = { "aloes", "kolagen", "kosmetyki", "suplementy", "urzadzenia", "true" };
             foreach(string wybor in spis)
             {
-
+                potwierdzenie = Spis_Produktow(potwierdzenie, wybor);
             }
             return potwierdzenie;
         }
-
         private static bool Spis_Produktow(bool potwierdzenie, string wybor)
         {
             switch(wybor)
             {
                 case "aloes":
                     string[] tablica_sciezka_aloes = { sciezka_aloe_vera_line, sciezka_aloesowy_sok };
+                    Baza_Produktow.Aloes = Wczytaj_produkty(Baza_Produktow.Aloes, tablica_sciezka_aloes);
                     break;
                 case "kolagen":
                     string[] tablica_sciezka_kolagen = { sciezka_produkty_kolagenowe, sciezka_kolagen_grafit_srebro_platyna, };
+                    Baza_Produktow.Kolagen = Wczytaj_produkty(Baza_Produktow.Kolagen, tablica_sciezka_kolagen);
                     break;
                 case "kosmetyki":
                     string[] tablica_sciezka_kosmetyki = { sciezka_kosmetyki, sciezka_produkty_kolagenowe, sciezka_kolagen_grafit_srebro_platyna };
+                    Baza_Produktow.Kosmetyki = Wczytaj_produkty(Baza_Produktow.Kosmetyki, tablica_sciezka_kosmetyki);
                     break;
                 case "suplementy":
                     string[] tablica_sciezka_suplementy = { sciezka_witaminy, sciezka_kolagen_kapsolki, sciezka_aloesowy_sok };
+                    Baza_Produktow.Suplementy = Wczytaj_produkty(Baza_Produktow.Suplementy, tablica_sciezka_suplementy);
                     break;
                 case "urzadzenia":
                     string[] tablica_sciezka_urzadzenia = { sciezka_urzadzenia };
+                    Baza_Produktow.Urzadzenia = Wczytaj_produkty(Baza_Produktow.Urzadzenia, tablica_sciezka_urzadzenia);
                     break;
                 case "true":
                     potwierdzenie = true;
@@ -66,33 +62,26 @@ namespace Planowanie_zamowienia_w_PC
             }
             return potwierdzenie;
         }
-        private static void wczytaj(ref Dictionary<Bindowanie.Klucz, Bindowanie.Zawartosc> slownik, string sciezka)
+        private static Dictionary<Baza_Produktow.Klucz, Baza_Produktow.Zawartosc> Wczytaj_produkty(Dictionary<Baza_Produktow.Klucz, Baza_Produktow.Zawartosc> slownik, string[] sciezka)
         {
             string pobierz = "";
             string dane = "";
-            decimal? cena = null;
+            decimal cena = 0.00M;
             decimal? ilosc = null;
-            StreamReader file = new StreamReader(sciezka, Encoding.UTF8);
-            while ((pobierz = file.ReadLine()) != null)
+            foreach(string wybierz in sciezka)
             {
-                Pobieranie(ref pobierz, out dane, out cena, out ilosc);
-                slownik.Add(new Bindowanie.Klucz(dane), new Bindowanie.Zawartosc(cena, ilosc));
+                StreamReader file = new StreamReader(wybierz, Encoding.UTF8);
+                while ((pobierz = file.ReadLine()) != null)
+                {
+                    Pobieranie(pobierz, out dane, out cena, out ilosc);      
+                    slownik.Add(new Baza_Produktow.Klucz(dane), new Baza_Produktow.Zawartosc(cena, ilosc));        
+                }                
+                file = Czyszczenie(file);             
             }
+            return slownik;
+            
         }
-        private static void zaladuj()
-        {
-            string[] ilosc = { sciezka_witaminy, sciezka_kolagen_kapsolki, sciezka_aloesowy_sok };
-            foreach(string ccc in ilosc)
-            {
-                wczytaj(ref Bindowanie.Suplementy, ccc);
-            }
-        }
-        private static StreamReader Czyszczenie(StreamReader czyszczenie_zmiennej)
-        {
-            czyszczenie_zmiennej = null;
-            return czyszczenie_zmiennej;
-        }
-        private static void Pobieranie(ref string dane, out string nazwa, out decimal? cena, out decimal? ilosc)
+        private static void Pobieranie(string dane, out string nazwa, out decimal cena, out decimal? ilosc)
         {
             nazwa = dane.Substring(0, dane.IndexOf(separator));
             string reszta = dane.Substring(dane.IndexOf(separator)+1);
@@ -106,117 +95,10 @@ namespace Planowanie_zamowienia_w_PC
                 ilosc = null;
             }
         }
-        private static void Wczytaj_suplementy()
+        private static StreamReader Czyszczenie(StreamReader czyszczenie_zmiennej)
         {
-            string pobierz = "";
-            string dane = "";
-            decimal? cena = null;
-            decimal? ilosc = null;
-            StreamReader file = new StreamReader(sciezka_witaminy, Encoding.UTF8);
-            while((pobierz = file.ReadLine()) != null)
-            {
-                Pobieranie(ref pobierz, out dane, out cena, out ilosc);
-                Bindowanie.Suplementy.Add(new Bindowanie.Klucz(dane), new Bindowanie.Zawartosc(cena, ilosc));
-            }
-            Czyszczenie(file);
-            file = new StreamReader(sciezka_kolagen_kapsolki, Encoding.UTF8);
-            while ((pobierz = file.ReadLine()) != null)
-            {
-                Pobieranie(ref pobierz, out dane, out cena, out ilosc);
-                Bindowanie.Suplementy.Add(new Bindowanie.Klucz(dane), new Bindowanie.Zawartosc(cena, ilosc));
-            }
-            Czyszczenie(file);
-            file = new StreamReader(sciezka_aloesowy_sok, Encoding.UTF8);
-            while ((pobierz = file.ReadLine()) != null)
-            {
-                Pobieranie(ref pobierz, out dane, out cena, out ilosc);
-                Bindowanie.Suplementy.Add(new Bindowanie.Klucz(dane), new Bindowanie.Zawartosc(cena, ilosc));
-            }
-        }
-
-        private static void Wczytaj_urzadzenia()
-        {
-            string pobierz = "";
-            string dane = "";
-            decimal? cena = null;
-            decimal? ilosc = null;
-            StreamReader file = new StreamReader(sciezka_urzadzenia, Encoding.UTF8);
-            while ((pobierz = file.ReadLine()) != null)
-            {
-                Pobieranie(ref pobierz, out dane, out cena, out _);
-                Bindowanie.Urzadzenia.Add(new Bindowanie.Klucz(dane), new Bindowanie.Zawartosc(cena, ilosc));
-            }
-        }
-
-        private static void Wczytaj_kosmetyki()
-        {
-            string pobierz = "";
-            string dane = "";
-            decimal? cena = null;
-            decimal? ilosc = null;
-            StreamReader file = new StreamReader(sciezka_kosmetyki, Encoding.UTF8);
-            while ((pobierz = file.ReadLine()) != null)
-            {
-                Pobieranie(ref pobierz, out dane, out cena, out ilosc);
-                Bindowanie.Kosmetyki.Add(new Bindowanie.Klucz(dane), new Bindowanie.Zawartosc(cena, ilosc));
-            }
-            Czyszczenie(file);
-            file = new StreamReader(sciezka_produkty_kolagenowe, Encoding.UTF8);
-            while ((pobierz = file.ReadLine()) != null)
-            {
-                Pobieranie(ref pobierz, out dane, out cena, out ilosc);
-                Bindowanie.Kosmetyki.Add(new Bindowanie.Klucz(dane), new Bindowanie.Zawartosc(cena, ilosc));
-            }
-            Czyszczenie(file);
-            file = new StreamReader(sciezka_kolagen_grafit_srebro_platyna, Encoding.UTF8);
-            while ((pobierz = file.ReadLine()) != null)
-            {
-                Pobieranie(ref pobierz, out dane, out cena, out ilosc);
-                Bindowanie.Kosmetyki.Add(new Bindowanie.Klucz(dane), new Bindowanie.Zawartosc(cena, ilosc));
-            }
-        }
-
-        private static void Wczytaj_aloes()
-        {
-            string pobierz = "";
-            string dane = "";
-            decimal? cena = null;
-            decimal? ilosc = null;
-            StreamReader file = new StreamReader(sciezka_aloe_vera_line, Encoding.UTF8);
-            while ((pobierz = file.ReadLine()) != null)
-            {
-                Pobieranie(ref pobierz, out dane, out cena, out _);
-                Bindowanie.Aloes.Add(new Bindowanie.Klucz(dane), new Bindowanie.Zawartosc(cena, ilosc));
-            }
-            Czyszczenie(file);
-            file = new StreamReader(sciezka_aloesowy_sok, Encoding.UTF8);
-            while ((pobierz = file.ReadLine()) != null)
-            {
-                Pobieranie(ref pobierz, out dane, out cena, out _);
-                Bindowanie.Aloes.Add(new Bindowanie.Klucz(dane), new Bindowanie.Zawartosc(cena, ilosc));
-            }
-        }
-
-        private static void Wczytaj_kolagen()
-        {
-            string pobierz = "";
-            string dane = "";
-            decimal? cena = 0;
-            decimal? ilosc = 0;
-            StreamReader file = new StreamReader(sciezka_produkty_kolagenowe, Encoding.UTF8);
-            while ((pobierz = file.ReadLine()) != null)
-            {
-                Pobieranie(ref pobierz, out dane, out cena, out _);
-                Bindowanie.Kolagen.Add(new Bindowanie.Klucz(dane), new Bindowanie.Zawartosc(cena, ilosc));
-            }
-            Czyszczenie(file);
-            file = new StreamReader(sciezka_kolagen_grafit_srebro_platyna, Encoding.UTF8);
-            while ((pobierz = file.ReadLine()) != null)
-            {
-                Pobieranie(ref pobierz, out dane, out cena, out _);
-                Bindowanie.Kolagen.Add(new Bindowanie.Klucz(dane), new Bindowanie.Zawartosc(cena, ilosc));
-            }
-            Czyszczenie(file);
+            czyszczenie_zmiennej = null;
+            return czyszczenie_zmiennej;
         }
     }
 }
