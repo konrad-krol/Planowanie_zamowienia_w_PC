63c1ee673ed716cf77e65720d6160e7e8228c987
Konrad Król <44756736+konrad-krol@users.noreply.github.com>
2019-09-17 20:15:11 +0200

    Create Baza_Koszyk.cs

---
+++ Planowanie_zamowienia_w_PC/Planowanie_zamowienia_w_PC/Baza_Koszyk.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planowanie_zamowienia_w_PC
+{
+    class Baza_Koszyk
+    {
+        public static Dictionary<Klucz, Zawartosc> Koszyk = new Dictionary<Klucz, Zawartosc>();
+        public class Klucz
+        {
+            public string Nazwa { get; set; }
+            public Klucz(string okNazwa)
+            {
+                Nazwa = okNazwa;
+            }
+        }
+        public class Zawartosc
+        {
+            public decimal Cena { get; set; }
+            public int Ilosc { get; set; }
+            public int Pojemnosc { get; set; }
+            public Zawartosc(decimal okCena, int okIlosc, int okPojemnosc)
+            {
+                Cena = okCena;
+                Ilosc = okIlosc;
+                Pojemnosc = okPojemnosc;
+            }
+        }
+    }
+}
