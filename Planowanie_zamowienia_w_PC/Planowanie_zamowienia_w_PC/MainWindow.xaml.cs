3fd90fb9be36dad24de02fe013f034745ff2fc2b
konradek281997 <konradek281997@users.noreply.github.com>
2019-09-20 00:26:46 +0200

    Update MainWindow.xaml.cs

--- Planowanie_zamowienia_w_PC/Planowanie_zamowienia_w_PC/MainWindow.xaml.cs
+++ Planowanie_zamowienia_w_PC/Planowanie_zamowienia_w_PC/MainWindow.xaml.cs
@@ -25,7 +25,11 @@ namespace Planowanie_zamowienia_w_PC
         {
             InitializeComponent();
             Lista_Suplementy.ItemsSource = Baza_Produktow.Suplementy;
-            
+            Lista_Kosmetyki.ItemsSource = Baza_Produktow.Kosmetyki;
+            Lista_Urzadzenia.ItemsSource = Baza_Produktow.Urzadzenia;
+            Lista_Aloes.ItemsSource = Baza_Produktow.Aloes;
+            Lista_Kolagen.ItemsSource = Baza_Produktow.Kolagen;
+            Lista_Koszyk.ItemsSource = Baza_Koszyk.Koszyk;
         }
         private void Znikanie_ekranow()
         {
@@ -33,12 +37,26 @@ namespace Planowanie_zamowienia_w_PC
             Ekran_Kategoria_Produktow.Visibility = Visibility.Collapsed;
             Ekran_Kategoria_Suplementy.Visibility = Visibility.Collapsed;
             Ekran_Produkty_W_Koszyku.Visibility = Visibility.Collapsed;
+            Ekran_Kategoria_Aloes.Visibility = Visibility.Collapsed;
+            Ekran_Kategoria_Kolagen.Visibility = Visibility.Collapsed;
+            Ekran_Kategoria_Kosmetyki.Visibility = Visibility.Collapsed;
+            Ekran_Kategoria_Urzadzenia.Visibility = Visibility.Collapsed;
+        }
+        private void Odswiezanie_List_Produktow()
+        {
+            Lista_Suplementy.Items.Refresh();
+            Lista_Urzadzenia.Items.Refresh();
+            Lista_Kosmetyki.Items.Refresh();
+            Lista_Kolagen.Items.Refresh();
+            Lista_Aloes.Items.Refresh();
+            Lista_Koszyk.Items.Refresh();
         }
         private void Przycisk_Rozpocznij_Click(object sender, RoutedEventArgs e)
         {
             bool potwierdzenie = false;
             try
             {
+                Baza_Koszyk.Koszyk = Baza_Koszyk.Czyszczenie_Koszyka(Baza_Koszyk.Koszyk);
                 potwierdzenie = Wczytanie_produktow.Wybor_wczytywanego_produktu(potwierdzenie);
             }
             catch(Exception exception)
@@ -49,29 +67,49 @@ namespace Planowanie_zamowienia_w_PC
             if(potwierdzenie == true)
             {
                 Znikanie_ekranow();
+                Odswiezanie_List_Produktow();
+                Ekran_Kategoria_Produktow.Visibility = Visibility.Visible;
             }
         }
-
         private void Przycisk_Dodaj_Do_Koszyka_Click(object sender, RoutedEventArgs e)
-        {
-            bool potwierdzenie = false;
-            potwierdzenie = Wczytanie_produktow.Wybor_wczytywanego_produktu(potwierdzenie);
-            Lista_Suplementy.Items.Refresh();
-        }
-
-        private void Przycisk_Suplementy_Click(object sender, RoutedEventArgs e)
-        {
-            Znikanie_ekranow();
-            Ekran_Kategoria_Suplementy.Visibility = Visibility.Visible;
-            Ekran_Produkty_W_Koszyku.Visibility = Visibility.Visible;
-        }
-
-        private void Przycisk_Powrot_Do_Kategorii_Produktow_Click(object sender, RoutedEventArgs e)
         {
             var sample = (KeyValuePair<Baza_Produktow.Klucz, Baza_Produktow.Zawartosc>)Lista_Suplementy.SelectedItem;
             string abc = sample.Key.Nazwa;
             decimal www = sample.Value.Cena;
             int? aaa = sample.Value.Ilosc;
+            Odswiezanie_List_Produktow();
+        }
+        private void Przycisk_Wybor_Ekranu_Click(object sender, RoutedEventArgs e)
+        {
+            Znikanie_ekranow();
+            Button Przycisk = (Button)sender;
+            switch (Przycisk.Content.ToString())
+            {
+                case "Suplementy diety":
+                    Ekran_Kategoria_Suplementy.Visibility = Visibility.Visible;
+                    break;
+                case "Urządzenia":
+                    Ekran_Kategoria_Urzadzenia.Visibility = Visibility.Visible;
+                    break;
+                case "Kosmetyki":
+                    Ekran_Kategoria_Kosmetyki.Visibility = Visibility.Visible;
+                    break;
+                case "Aloe Vera Line":
+                    Ekran_Kategoria_Aloes.Visibility = Visibility.Visible;
+                    break;
+                case "Kolagen":
+                    Ekran_Kategoria_Kolagen.Visibility = Visibility.Visible;
+                    break;
+                case "Wstecz":
+                    Ekran_Kategoria_Produktow.Visibility = Visibility.Visible;
+                    break;
+                case "Idź do koszyka":
+                    Ekran_Produkty_W_Koszyku.Visibility = Visibility.Visible;
+                    break;
+                default:
+                    MessageBox.Show("Nieznana operacja!", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+            }
         }
     }
 }
