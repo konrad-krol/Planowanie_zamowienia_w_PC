2ad6594a25bb624c48aa9c0a783869c9e5d4eed5
konradek281997 <konradek281997@users.noreply.github.com>
2019-09-24 00:07:35 +0200

    Update Obsluga_Koszyk.cs

--- Planowanie_zamowienia_w_PC/Planowanie_zamowienia_w_PC/Obsluga_Koszyk.cs
+++ Planowanie_zamowienia_w_PC/Planowanie_zamowienia_w_PC/Obsluga_Koszyk.cs
@@ -8,20 +8,25 @@ namespace Planowanie_zamowienia_w_PC
 {
     class Obsluga_Koszyk
     {
-        public static int ilosc_produktow_do_koszyka = new int ();
+        public static int liczba_produktow_do_dodania_koszyka = 0;
+        public static decimal kwota_zamowienia = 0.00M;
+        public static int liczba_produktow_w_koszyku = 0;
 
-        public class Liczba_Produktow_Do_Koszyka
-        {
-            public int Liczba_do_dodania { get; set; }
-            public Liczba_Produktow_Do_Koszyka(int ok_Liczba_Produktow_Do_Koszyka)
-            {
-                Liczba_do_dodania = ok_Liczba_Produktow_Do_Koszyka;
-            }
-        }
         public static void Dodaj_Do_Koszyka(string nazwa_produktu, decimal cena_produktow, int ilosc, int? pojemnosc)
         {
             cena_produktow *= ilosc;
+            kwota_zamowienia += cena_produktow;
+            liczba_produktow_w_koszyku += ilosc;
             Baza_Koszyk.Koszyk.Add(new Baza_Koszyk.Klucz(nazwa_produktu), new Baza_Koszyk.Zawartosc(cena_produktow, ilosc, pojemnosc));
+        }
+        public static void Usuwanie_Z_Koszyka(Baza_Koszyk.Klucz klucz)
+        {
+            Baza_Koszyk.Koszyk.TryGetValue(klucz, out var pobierz_dane);
+            decimal cena = pobierz_dane.Cena;
+            int ilosc = pobierz_dane.Ilosc;
+            kwota_zamowienia -= cena;
+            liczba_produktow_w_koszyku -= ilosc;
+            Baza_Koszyk.Koszyk.Remove(klucz);
         }
     }
 }
