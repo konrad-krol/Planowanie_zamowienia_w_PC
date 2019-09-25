e2e7a5d644d5ab7af6dda27ceb9cd04973245a42
konradek281997 <konradek281997@users.noreply.github.com>
2019-09-26 00:11:49 +0200

    Update Obsluga_Koszyk.cs

--- Planowanie_zamowienia_w_PC/Planowanie_zamowienia_w_PC/Obsluga_Koszyk.cs
+++ Planowanie_zamowienia_w_PC/Planowanie_zamowienia_w_PC/Obsluga_Koszyk.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace Planowanie_zamowienia_w_PC
 {
@@ -12,21 +15,37 @@ namespace Planowanie_zamowienia_w_PC
         public static decimal kwota_zamowienia = 0.00M;
         public static int liczba_produktow_w_koszyku = 0;
 
-        public static void Dodaj_Do_Koszyka(string nazwa_produktu, decimal cena_produktow, int ilosc, int? pojemnosc)
+        public static void Dodaj_Do_Koszyka(Baza_Produktow.Klucz klucz, Baza_Produktow.Zawartosc zawartosc, int ilosc)
         {
-            cena_produktow *= ilosc;
+            /*decimal abc = 1234;
+            Baza_Produktow.Koszyk.Add(new Baza_Produktow.Klucz(klucz.Nazwa), new Baza_Produktow.Zawartosc_Koszyk(abc, ilosc, zawartosc.Pojemnosc));
+
+            Baza_Produktow.Koszyk.TryGetValue(klucz, out var pobierz_dane);
+            int? kkk = pobierz_dane.Ilosc;*/
+            bool abc = Sprawdz_Koszyk(klucz);
+            decimal cena_produktow = ilosc*zawartosc.Cena;
             kwota_zamowienia += cena_produktow;
             liczba_produktow_w_koszyku += ilosc;
-            Baza_Koszyk.Koszyk.Add(new Baza_Koszyk.Klucz(nazwa_produktu), new Baza_Koszyk.Zawartosc(cena_produktow, ilosc, pojemnosc));
+            Baza_Produktow.Koszyk.Add(new Baza_Produktow.Klucz(klucz.Nazwa), new Baza_Produktow.Zawartosc_Koszyk(cena_produktow, ilosc, zawartosc.Pojemnosc));
+            
         }
-        public static void Usuwanie_Z_Koszyka(Baza_Koszyk.Klucz klucz)
+        public static bool Sprawdz_Koszyk(Baza_Produktow.Klucz klucz)
         {
-            Baza_Koszyk.Koszyk.TryGetValue(klucz, out var pobierz_dane);
+            Baza_Produktow.Klucz abc = klucz;
+            if (Baza_Produktow.Koszyk.ContainsKey(abc))
+                return false;
+            else
+                return true;
+
+        }
+        public static void Usuwanie_Z_Koszyka(Baza_Produktow.Klucz klucz)
+        {
+            Baza_Produktow.Koszyk.TryGetValue(klucz, out var pobierz_dane);
             decimal cena = pobierz_dane.Cena;
             int ilosc = pobierz_dane.Ilosc;
             kwota_zamowienia -= cena;
             liczba_produktow_w_koszyku -= ilosc;
-            Baza_Koszyk.Koszyk.Remove(klucz);
+            Baza_Produktow.Koszyk.Remove(klucz);
         }
     }
 }
